147885dd9306609278b9af6897a53e8a12a78a9a
uuganbold <uuganhuu@gmail.com>
2020-02-16 11:08:21 -0600

    import css in scontentsciript

--- webpack.config.js
+++ webpack.config.js
@@ -3,12 +3,14 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { optimize } = require('webpack');
 const { join } = require('path');
 let prodPlugins = [];
+
 if (process.env.NODE_ENV === 'production') {
   prodPlugins.push(
     new optimize.AggressiveMergingPlugin(),
     new optimize.OccurrenceOrderPlugin()
   );
 }
+
 module.exports = {
   mode: process.env.NODE_ENV,
   devtool: 'inline-source-map',
